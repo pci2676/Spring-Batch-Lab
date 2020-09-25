71e3ce70f06b242d1a9e052d7944d5f9829ec6ee
chanin <pci2676@gmail.com>
2020-09-25 22:13:44 +0900

    project init : simple batch application

---
+++ bombatch-simple-batch/src/main/java/com/javabom/bombatch/SimpleBatchApplication.java
@@ -0,0 +1,13 @@
+package com.javabom.bombatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SimpleBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBatchApplication.class, args);
+    }
+}
